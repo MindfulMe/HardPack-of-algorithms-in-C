a9ec4c78ab669b1abe9cb995e907f4aa6b2bdc4d
Jack Raider                              (Rostislav Gogolauri) <go.go.gg.rostislav@gmail.com>
2018-01-20 00:29:36 +0200

    Add files via upload

---
+++ SearchingAlgoritms(Structures)/LinearSearchingAlgorithm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace doggy
+{   
+    using System;
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string pattern = Console.ReadLine();
+            int k = Convert.ToInt32(Console.ReadLine());
+            string[] datab = new string[k];
+            for (int i = 0; i < k; i++)
+            {
+                datab[i] = Console.ReadLine();
+            }
+            for(int f = 0; f<k;f++)
+            {
+                if (datab[f] == pattern)
+                {
+                    Console.WriteLine("Found!");
+                    Console.WriteLine("  at the index: {0}", f);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
