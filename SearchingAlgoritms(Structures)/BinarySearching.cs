9112d1a935510039c77d397f6dbb5e84f29203f3
Jack Raider                              (Rostislav Gogolauri) <go.go.gg.rostislav@gmail.com>
2018-01-20 02:02:35 +0200

    Add files via upload

---
+++ SearchingAlgoritms(Structures)/BinarySearching.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Binary
+{
+    class Program
+    {
+        static int binarySearch(int[] arr, int l, int r, int x)
+        {
+            if (r >= l)
+            {
+                int mid = l + (r - l) / 2;
+                if (arr[mid] == x)
+                    return mid;
+                if (arr[mid] > x)
+                    return binarySearch(arr, l, mid - 1, x);
+                else
+                    return binarySearch(arr, mid + 1, r, x);
+            }
+            return -1;
+        }
+
+
+
+
+        public static void Main(string[] args)
+        {
+            int[] arr = { 3, 4, 23, 42, 77, 100 };
+            int x = 42;
+            int result = binarySearch(arr, 0, arr.Length - 1, x);
+            if (result == -1)
+                Console.WriteLine("Element not found");
+            else
+                Console.WriteLine("Element found at the index " + result);
+            Console.ReadKey();
+        }
+    }
+}
+
